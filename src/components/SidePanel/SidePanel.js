98a52c2c5ad986f0bc7c0341e99b7697480109c5
Mariann <mariann19@gmail.com>
2020-02-22 22:05:03 -0500

    starred channel step 1

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -4,6 +4,7 @@ import { Menu } from 'semantic-ui-react'
 import UserPanel from './UserPanel'
 import Channels from './Channels'
 import DirectMessages from "./DirectMessages";
+import Starred from './Starred'
 
 class SidePanel extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class SidePanel extends React.Component {
     return (
       <Menu size='large' inverted fixed='left' vertical style={{ background: "#4c3c4c", fontSize: '1.2rem' }}>
         <UserPanel currentUser={currentUser} />
+        <Starred />
         <Channels currentUser={currentUser} />
         <DirectMessages currentUser={currentUser} />
       </Menu>

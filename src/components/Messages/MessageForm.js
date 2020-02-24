3e27f107c32558d8e9f7b720175c674901def0d4
Mariann <mariann19@gmail.com>
2020-02-24 18:55:49 -0500

    display animation while user is typing

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -1,32 +1,49 @@
-import React from 'react'
-import uuidv4 from 'uuid/v4'
-import firebase from '../../firebase'
-import { Segment, Button, Input } from 'semantic-ui-react'
+import React from 'react';
+import uuidv4 from 'uuid/v4';
+import firebase from '../../firebase';
+import { Segment, Button, Input } from 'semantic-ui-react';
 
-import FileModal from './FileModal'
-import ProgressBar from './ProgressBar'
+import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
     storageRef: firebase.storage().ref(),
-    uploadState: '',
+    typingRef: firebase.database().ref('typing'),
     uploadTask: null,
+    uploadState: '',
     percentUploaded: 0,
     message: '',
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
     errors: [],
-    modal: false,
-  }
+    modal: false
+  };
 
-  openModal = () => this.setState({ modal: true })
+  openModal = () => this.setState({ modal: true });
 
-  closeModal = () => this.setState({ modal: false })
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value })
-  }
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleKeyDown = () => {
+    const { message, typingRef, channel, user } = this.state;
+
+    if (message) {
+      typingRef
+        .child(channel.id)
+        .child(user.uid)
+        .set(user.displayName);
+    } else {
+      typingRef
+        .child(channel.id)
+        .child(user.uid)
+        .remove();
+    }
+  };
 
   createMessage = (fileUrl = null) => {
     const message = {
@@ -35,120 +52,142 @@ class MessageForm extends React.Component {
         id: this.state.user.uid,
         name: this.state.user.displayName,
         avatar: this.state.user.photoURL
-      },
-
-    }
-
+      }
+    };
     if (fileUrl !== null) {
-      message['image'] = fileUrl
+      message['image'] = fileUrl;
     } else {
-      message['content'] = this.state.message
+      message['content'] = this.state.message;
     }
-    return message
-  }
+    return message;
+  };
 
   sendMessage = () => {
-    const { getMessagesRef } = this.props
-    const { message, channel } = this.state
+    const { getMessagesRef } = this.props;
+    const { message, channel, user, typingRef } = this.state;
 
     if (message) {
-      this.setState({ loading: true })
+      this.setState({ loading: true });
       getMessagesRef()
         .child(channel.id)
         .push()
         .set(this.createMessage())
         .then(() => {
-          this.setState({ loading: false, message: '', errors: [] })
+          this.setState({
+            loading: false, message: '', errors: []
+          });
+          typingRef
+            .child(channel.id)
+            .child(user.uid)
+            .remove();
         })
         .catch(err => {
-          console.error(err)
+          console.error(err);
           this.setState({
             loading: false,
             errors: this.state.errors.concat(err)
-          })
-        })
+          });
+        });
     } else {
       this.setState({
         errors: this.state.errors.concat({ message: 'Add a message' })
-      })
+      });
     }
-  }
+  };
 
   getPath = () => {
     if (this.props.isPrivateChannel) {
-      return `chat/private-${this.state.channel.id}`
+      return `chat/private-${this.state.channel.id}`;
     } else {
-      return 'chat/public'
+      return 'chat/public';
     }
-  }
+  };
 
   uploadFile = (file, metadata) => {
-    const pathToUpload = this.state.channel.id
-    const ref = this.props.getMessagesRef()
-    const filePath = `${this.getPath()}/${uuidv4()}.jpg`
-    this.setState({
-      uploadState: 'uploading',
-      uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
-    }, () => {
-      this.state.uploadTask.on('state_changed', snap => {
-        const percentUploaded = Math.round((snap.bytesTransferred / snap.totalBytes) * 100)
-        this.props.isProgressBarVisible(percentUploaded)
-        this.setState({ percentUploaded })
-      }, err => {
-        console.error(err);
-        this.setState({
-          errors: this.state.errors.concat(err),
-          uploadState: 'error',
-          uploadTask: null
-        })
-      }, () => {
-        this.state.uploadTask.snapshot.ref.getDownloadURL().then(downloadUrl => {
-          this.sendFileMessage(downloadUrl, ref, pathToUpload)
-        })
-          .catch(err => {
-            console.error(err)
+    const pathToUpload = this.state.channel.id;
+    const ref = this.props.getMessagesRef();
+    const filePath = `${this.getPath()}/${uuidv4()}.jpg`;
+
+    this.setState(
+      {
+        uploadState: 'uploading',
+        uploadTask: this.state.storageRef.child(filePath).put(file, metadata)
+      },
+      () => {
+        this.state.uploadTask.on(
+          'state_changed',
+          snap => {
+            const percentUploaded = Math.round(
+              (snap.bytesTransferred / snap.totalBytes) * 100
+            );
+            this.setState({ percentUploaded });
+          },
+          err => {
+            console.error(err);
             this.setState({
               errors: this.state.errors.concat(err),
               uploadState: 'error',
               uploadTask: null
-            })
-          })
-      })
-    })
-  }
+            });
+          },
+          () => {
+            this.state.uploadTask.snapshot.ref
+              .getDownloadURL()
+              .then(downloadUrl => {
+                this.sendFileMessage(downloadUrl, ref, pathToUpload);
+              })
+              .catch(err => {
+                console.error(err);
+                this.setState({
+                  errors: this.state.errors.concat(err),
+                  uploadState: 'error',
+                  uploadTask: null
+                });
+              });
+          }
+        );
+      }
+    );
+  };
 
   sendFileMessage = (fileUrl, ref, pathToUpload) => {
-    ref.child(pathToUpload)
+    ref
+      .child(pathToUpload)
       .push()
       .set(this.createMessage(fileUrl))
       .then(() => {
-        this.setState({ uploadState: 'done' })
+        this.setState({ uploadState: 'done' });
       })
       .catch(err => {
-        console.error(err)
+        console.error(err);
         this.setState({
           errors: this.state.errors.concat(err)
-        })
-      })
-  }
+        });
+      });
+  };
 
   render() {
-    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state
+    // prettier-ignore
+    const { errors, message, loading, modal, uploadState, percentUploaded } = this.state;
 
     return (
       <Segment className='message__form'>
         <Input
           fluid
           name='message'
-          style={{ marginBottom: '0.7em' }}
-          value={message}
           onChange={this.handleChange}
+          onKeyDown={this.handleKeyDown}
+          value={message}
+          style={{ marginBottom: '0.7em' }}
           label={<Button icon={'add'} />}
           labelPosition='left'
           className={
-            errors.some(error => error.message.includes('message')) ? 'error' : ''
+            errors.some(error => error.message.includes('message'))
+              ? 'error'
+              : ''
           }
-          placeholder='Write your message' />
+          placeholder='Write your message'
+        />
         <Button.Group icon widths='2'>
           <Button
             onClick={this.sendMessage}
@@ -163,7 +202,7 @@ class MessageForm extends React.Component {
             disabled={uploadState === 'uploading'}
             onClick={this.openModal}
             content='Upload Media'
-            labelPosition="right"
+            labelPosition='right'
             icon='cloud upload'
           />
         </Button.Group>
@@ -174,10 +213,11 @@ class MessageForm extends React.Component {
         />
         <ProgressBar
           uploadState={uploadState}
-          percentUploaded={percentUploaded} />
+          percentUploaded={percentUploaded}
+        />
       </Segment>
-    )
+    );
   }
 }
 
-export default MessageForm
+export default MessageForm;
--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React from 'react';
 import firebase from '../../firebase';
 import { connect } from 'react-redux';
-import { setCurrentChannel, setPrivateChannel } from '../../actions'
-import { Menu, Icon, Modal, Form, Input, Button, Label } from "semantic-ui-react";
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
+// prettier-ignore
+import { Menu, Icon, Modal, Form, Input, Button, Label } from 'semantic-ui-react';
 
 class Channels extends React.Component {
   state = {
     activeChannel: '',
     user: this.props.currentUser,
-    channel: '',
+    channel: null,
     channels: [],
     channelName: '',
     channelDetails: '',
     channelsRef: firebase.database().ref('channels'),
     messagesRef: firebase.database().ref('messages'),
+    typingRef: firebase.database().ref('typing'),
     notifications: [],
     modal: false,
     firstLoad: true
@@ -29,57 +31,67 @@ class Channels extends React.Component {
 
   addListeners = () => {
     let loadedChannels = [];
-    this.state.channelsRef.on("child_added", snap => {
+    this.state.channelsRef.on('child_added', snap => {
       loadedChannels.push(snap.val());
       this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
-      this.addNotificationListener(snap.key)
+      this.addNotificationListener(snap.key);
     });
   };
 
   addNotificationListener = channelId => {
     this.state.messagesRef.child(channelId).on('value', snap => {
       if (this.state.channel) {
-        this.handleNotifications(channelId, this.state.channel.id, this.state.notifications, snap)
+        this.handleNotifications(
+          channelId,
+          this.state.channel.id,
+          this.state.notifications,
+          snap
+        );
       }
-    })
-  }
+    });
+  };
 
   handleNotifications = (channelId, currentChannelId, notifications, snap) => {
-    let lastTotal = 0
+    let lastTotal = 0;
 
-    let index = notifications.findIndex(notification => notification.id === channelId)
+    let index = notifications.findIndex(
+      notification => notification.id === channelId
+    );
+
     if (index !== -1) {
       if (channelId !== currentChannelId) {
         lastTotal = notifications[index].total;
+
         if (snap.numChildren() - lastTotal > 0) {
-          notifications[index].count = snap.numChildren() - lastTotal
+          notifications[index].count = snap.numChildren() - lastTotal;
         }
       }
-      notifications[index].lastKnownTotal = snap.numChildren()
+      notifications[index].lastKnownTotal = snap.numChildren();
     } else {
       notifications.push({
         id: channelId,
         total: snap.numChildren(),
         lastKnownTotal: snap.numChildren(),
         count: 0
-      })
+      });
     }
-    this.setState({ notifications })
-  }
+
+    this.setState({ notifications });
+  };
 
   removeListeners = () => {
-    this.state.channelsRef.off()
-  }
+    this.state.channelsRef.off();
+  };
 
   setFirstChannel = () => {
-    const firtsChannel = this.state.channels[0]
+    const firstChannel = this.state.channels[0];
     if (this.state.firstLoad && this.state.channels.length > 0) {
-      this.props.setCurrentChannel(firtsChannel)
-      this.setActiveChannel(firtsChannel)
-      this.setState({ channel: firtsChannel })
+      this.props.setCurrentChannel(firstChannel);
+      this.setActiveChannel(firstChannel);
+      this.setState({ channel: firstChannel });
     }
-    this.setState({ firstLoad: false })
-  }
+    this.setState({ firstLoad: false });
+  };
 
   addChannel = () => {
     const { channelsRef, channelName, channelDetails, user } = this.state;
@@ -100,9 +112,9 @@ class Channels extends React.Component {
       .child(key)
       .update(newChannel)
       .then(() => {
-        this.setState({ channelName: "", channelDetails: "" });
+        this.setState({ channelName: '', channelDetails: '' });
         this.closeModal();
-        console.log("channel added");
+        console.log('channel added');
       })
       .catch(err => {
         console.error(err);
@@ -121,38 +133,47 @@ class Channels extends React.Component {
   };
 
   changeChannel = channel => {
-    this.setActiveChannel(channel)
-    this.clearNotifications()
-    this.props.setCurrentChannel(channel)
-    this.props.setPrivateChannel(false)
-    this.setState({ channel })
-  }
+    this.setActiveChannel(channel);
+    this.state.typingRef
+      .child(this.state.channel.id)
+      .child(this.state.user.uid)
+      .remove();
+    this.clearNotifications();
+    this.props.setCurrentChannel(channel);
+    this.props.setPrivateChannel(false);
+    this.setState({ channel });
+  };
 
   clearNotifications = () => {
-    let index = this.state.notifications.findIndex(notification => notification.id === this.state.channel.id)
+    let index = this.state.notifications.findIndex(
+      notification => notification.id === this.state.channel.id
+    );
+
     if (index !== -1) {
-      let updatedNotifications = [...this.state.notifications]
-      updatedNotifications[index].total = this.state.notifications[index].lastKnownTotal
-      updatedNotifications[index].count = 0
-      this.setState({ notifications: updatedNotifications })
+      let updatedNotifications = [...this.state.notifications];
+      updatedNotifications[index].total = this.state.notifications[
+        index
+      ].lastKnownTotal;
+      updatedNotifications[index].count = 0;
+      this.setState({ notifications: updatedNotifications });
     }
-  }
+  };
 
   setActiveChannel = channel => {
-    this.setState({ activeChannel: channel.id })
-  }
+    this.setState({ activeChannel: channel.id });
+  };
 
-  getNotificatonCount = channel => {
-    let count = 0
+  getNotificationCount = channel => {
+    let count = 0;
 
     this.state.notifications.forEach(notification => {
       if (notification.id === channel.id) {
-        count = notification.count
+        count = notification.count;
       }
     });
 
-    if (count > 0) return count
-  }
+    if (count > 0) return count;
+  };
 
   displayChannels = channels =>
     channels.length > 0 &&
@@ -164,11 +185,11 @@ class Channels extends React.Component {
         style={{ opacity: 0.7 }}
         active={channel.id === this.state.activeChannel}
       >
-        {this.getNotificatonCount(channel) && (
-          <Label color='red'>{this.getNotificatonCount(channel)}</Label>
+        {this.getNotificationCount(channel) && (
+          <Label color='red'>{this.getNotificationCount(channel)}</Label>
         )}
         # {channel.name}
-      </Menu.Item >
+      </Menu.Item>
     ));
 
   isFormValid = ({ channelName, channelDetails }) =>
@@ -186,9 +207,9 @@ class Channels extends React.Component {
         <Menu.Menu className='menu'>
           <Menu.Item>
             <span>
-              <Icon name="exchange" /> CHANNELS
-            </span>{" "}
-            ({channels.length}) <Icon name="add" onClick={this.openModal} />
+              <Icon name='exchange' /> CHANNELS
+            </span>{' '}
+            ({channels.length}) <Icon name='add' onClick={this.openModal} />
           </Menu.Item>
           {this.displayChannels(channels)}
         </Menu.Menu>
@@ -201,8 +222,8 @@ class Channels extends React.Component {
               <Form.Field>
                 <Input
                   fluid
-                  label="Name of Channel"
-                  name="channelName"
+                  label='Name of Channel'
+                  name='channelName'
                   onChange={this.handleChange}
                 />
               </Form.Field>
@@ -210,8 +231,8 @@ class Channels extends React.Component {
               <Form.Field>
                 <Input
                   fluid
-                  label="About the Channel"
-                  name="channelDetails"
+                  label='About the Channel'
+                  name='channelDetails'
                   onChange={this.handleChange}
                 />
               </Form.Field>
@@ -219,11 +240,11 @@ class Channels extends React.Component {
           </Modal.Content>
 
           <Modal.Actions>
-            <Button color="green" inverted onClick={this.handleSubmit}>
-              <Icon name="checkmark" /> Add
+            <Button color='green' inverted onClick={this.handleSubmit}>
+              <Icon name='checkmark' /> Add
             </Button>
-            <Button color="red" inverted onClick={this.closeModal}>
-              <Icon name="remove" /> Cancel
+            <Button color='red' inverted onClick={this.closeModal}>
+              <Icon name='remove' /> Cancel
             </Button>
           </Modal.Actions>
         </Modal>
@@ -232,4 +253,7 @@ class Channels extends React.Component {
   }
 }
 
-export default connect(null, { setCurrentChannel, setPrivateChannel })(Channels);
+export default connect(
+  null,
+  { setCurrentChannel, setPrivateChannel }
+)(Channels);
